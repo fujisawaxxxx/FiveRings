e9d4b9edb5131e30aa67ba17b3d03e3bb9cdd49e
a_fujisawa@ag-media.co.jp <a_fujisawa@ag-media.co.jp>
2024-08-29 10:11:09 +0900

    メイン画面作成

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
+from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('main/', views.main_view, name='main'),  # メイン画面へのURL
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required  # ログインが必要なビューとして設定
+def main_view(request):
+    return render(request, 'accounts/main.html')

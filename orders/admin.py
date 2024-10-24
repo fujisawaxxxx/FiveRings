b82f3e9a5687608de90d500aa6dddbf61612b0c1
a_fujisawa@ag-media.co.jp <a_fujisawa@ag-media.co.jp>
2024-10-24 11:11:22 +0900

    メインページにプルダウン追加　５商品の各テーブルに部数単価を入力（後で見なおす）

--- orders/admin.py
+++ orders/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import AgreementColor
 from .models import AgreementMonochrome
 from .models import CaseCard
+from .models import Chiken
+from .models import Fukuyaku
 
 admin.site.register(AgreementColor)
 admin.site.register(AgreementMonochrome)
 admin.site.register(CaseCard)
+admin.site.register(Chiken)
+admin.site.register(Fukuyaku)
--- orders/models.py
+++ orders/models.py
@@ -8,8 +8,8 @@ class AgreementColor(models.Model):
         return f"{self.number_of_sheets}枚 - {self.unit_price}円"
 
     class Meta:
-        verbose_name = "同意説明書カラー"
-        verbose_name_plural = "同意説明書カラー"
+        verbose_name = "同意説明書（カラー）"
+        verbose_name_plural = "同意説明書（カラー）"
 
 class AgreementMonochrome (models.Model):
     number_of_sheets = models.IntegerField(verbose_name="枚数")
@@ -19,8 +19,8 @@ class AgreementMonochrome (models.Model):
         return f"{self.number_of_sheets}枚 - {self.unit_price}円"
 
     class Meta:
-        verbose_name = "同意説明書モノクロ"
-        verbose_name_plural = "同意説明書モノクロ"
+        verbose_name = "同意説明書（モノクロ）"
+        verbose_name_plural = "同意説明書（モノクロ）"
 
 class CaseCard (models.Model):
     number_of_sheets = models.IntegerField(verbose_name="枚数")
@@ -30,8 +30,8 @@ class CaseCard (models.Model):
         return f"{self.number_of_sheets}枚 - {self.unit_price}円"
 
     class Meta:
-        verbose_name = "ケースカード"
-        verbose_name_plural = "ケースカード"
+        verbose_name = "ケースカード（症例報告書）"
+        verbose_name_plural = "ケースカード（症例報告書）"
 
 class Chiken (models.Model):
     number_of_sheets = models.IntegerField(verbose_name="枚数")
@@ -41,6 +41,17 @@ class Chiken (models.Model):
         return f"{self.number_of_sheets}枚 - {self.unit_price}円"
 
     class Meta:
-        verbose_name = "治験計画書"
-        verbose_name_plural = "治験計画書"
+        verbose_name = "治験実施計画書"
+        verbose_name_plural = "治験実施計画書"
+
+class Fukuyaku (models.Model):
+    number_of_sheets = models.IntegerField(verbose_name="枚数")
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="単価")
+
+    def __str__(self):
+        return f"{self.number_of_sheets}枚 - {self.unit_price}円"
+
+    class Meta:
+        verbose_name = "服薬日誌"
+        verbose_name_plural = "服薬日誌"
 

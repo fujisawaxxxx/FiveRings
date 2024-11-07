1f882863a68aab962fd62af1716f0457bc8b527e
a_fujisawa@ag-media.co.jp <a_fujisawa@ag-media.co.jp>
2024-11-07 13:36:38 +0900

    治験実施計画書、服薬日誌の価格を取得できるように

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     # ケースカードの価格取得API
     path('api/casecard-price/', views.get_case_card_price, name='get_case_card_price'),
+    # 治験実施計画書の価格取得API
+    path('api/chiken-plan-price/', views.get_chiken_plan_price, name='get_chiken_plan_price'),
+    # 服薬日誌の価格取得API
+    path('api/medication-diary-price/', views.get_medication_diary_price, name='get_medication_diary_price'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import JsonResponse
-from .models import CaseCard
+from .models import CaseCard,Chiken,Fukuyaku
 
+# ケースカード
 def get_case_card_price(request):
     content = request.GET.get('content', None)
 
@@ -13,6 +14,35 @@ def get_case_card_price(request):
             case_card = CaseCard.objects.get(number_of_sheets=content)
             return JsonResponse({'unit_price': case_card.unit_price})
         except CaseCard.DoesNotExist:
-            return JsonResponse({'error': 'No matching case card found'}, status=404)
+            return JsonResponse({'error': 'ケースカードが見つかりません'}, status=404)
     else:
-        return JsonResponse({'error': 'Content parameter missing'}, status=400)
+        return JsonResponse({'error': 'ケースカードのパラメーターが見つかりました'}, status=400)
+
+
+# 治験実施計画書
+def get_chiken_plan_price(request):
+    content = request.GET.get('content', None)
+
+    # contentが指定されている場合、そのnumber_of_sheetsに一致するケースカードを取得
+    if content is not None:
+        try:
+            chiken_plan = Chiken.objects.get(number_of_sheets=content)
+            return JsonResponse({'unit_price': chiken_plan.unit_price})
+        except Chiken.DoesNotExist:
+            return JsonResponse({'error': '治験実施計画書が見つかりません'}, status=404)
+    else:
+        return JsonResponse({'error': '治験実施計画書のパラメーターが見つかりました'}, status=400)
+
+# 服薬日誌
+def get_medication_diary_price(request):
+    content = request.GET.get('content', None)
+
+    # contentが指定されている場合、そのnumber_of_sheetsに一致するケースカードを取得
+    if content is not None:
+        try:
+            medication_diary = Fukuyaku.objects.get(number_of_sheets=content)
+            return JsonResponse({'unit_price': medication_diary.unit_price})
+        except Fukuyaku.DoesNotExist:
+            return JsonResponse({'error': '服薬日誌が見つかりません'}, status=404)
+    else:
+        return JsonResponse({'error': '服薬日誌のパラメーターが見つかりました'}, status=400)

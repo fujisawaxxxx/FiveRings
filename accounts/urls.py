aa5d465a33d0310ae384f8d97a97264dd9ccd2bf
a_fujisawa@ag-media.co.jp <a_fujisawa@ag-media.co.jp>
2024-11-27 16:48:27 +0900

    注文確認画面作成（複写オプションのデータの遷移はまだ）

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
+# accounts/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('main/', views.main_view, name='main'),  # メイン画面へのURL
+    path('create_order/', views.create_order_view, name='create_order'),  # 注文確認画面へのURL
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,26 @@
+# accounts/views.py
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required  # ログインが必要なビューとして設定
 def main_view(request):
     return render(request, 'accounts/main.html')
+
+@login_required  # 注文書作成画面へのビュー
+def create_order_view(request):
+    # 入力データをリクエストから取得
+    product_type = request.GET.get('product_type', '選択されていません')
+    quantity = request.GET.get('quantity', '-')
+    content = request.GET.get('content', '-')
+    unit_price = request.GET.get('unit_price', '-')
+    estimate_result = request.GET.get('estimate_result', '-')
+
+    context = {
+        'product_type': product_type,
+        'quantity': quantity,
+        'content': content,
+        'unit_price': unit_price,
+        'estimate_result': estimate_result,
+    }
+
+    return render(request, 'accounts/create_order.html', context)

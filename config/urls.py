4b1b4458546ed985c6a920b5fb88719317b4e0be
a_fujisawa@ag-media.co.jp <a_fujisawa@ag-media.co.jp>
2024-11-28 11:25:26 +0900

    runserverクリック時にaccounts/login.htmlにダイレクトでつながる

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect  # リダイレクトを行うために必要
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  # 正しい位置に記述されていますか？
     path('orders/', include('orders.urls')),      # ordersアプリのルーティング
+
+    path('', lambda request: redirect('login')),  # ルートURLを /accounts/login/ にリダイレクト
 ]

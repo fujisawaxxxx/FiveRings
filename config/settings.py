abcc634f38da2892d4910716ff66e83e5e14e210
a_fujisawa@ag-media.co.jp <a_fujisawa@ag-media.co.jp>
2025-03-31 11:57:53 +0900

    データベース設定をSQLiteからPostgreSQLに変更し、接続情報を追加

--- config/settings.py
+++ config/settings.py
@@ -78,9 +78,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "FiveRings_DB",
+        "USER": "a_fujisawa",
+        "PASSWORD": "51s93c7w",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
